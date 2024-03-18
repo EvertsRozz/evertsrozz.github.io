f9f616b8d84bf2d9069bc781e7a835f0d44191bf
Reportieris <evertrozenbaum@gmail.com>
2024-03-18 05:12:40 +0200

    initial stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./hero/hero.component').then(mod=> mod.HeroComponent)
+    }
+];
